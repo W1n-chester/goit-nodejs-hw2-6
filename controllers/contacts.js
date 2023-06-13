424072b17208345762d590a917e592c7c9e8cad6
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-06-13 21:37:28 +0300

    done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,9 +4,9 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  // const { page = 1, limit = 10 } = req.query;
-  // const skip = (page - 1) * limit;
-  const result = await Contact.find({owner}).populate("owner", "email subscription");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({owner}, null, {skip, limit}).populate("owner", "email subscription");
   res.json(result)
 };
 
