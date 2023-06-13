ba57b10c97c4a3db225c6c37846d3f8fa742d5ff
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-06-13 21:22:16 +0300

    mini fix
    не работает пагинация

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,9 +4,9 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
-  const result = await Contact.find({owner}, {skip, limit}).populate("owner", "email subscription");
+  // const { page = 1, limit = 10 } = req.query;
+  // const skip = (page - 1) * limit;
+  const result = await Contact.find({owner}).populate("owner", "email subscription");
   res.json(result)
 };
 
@@ -32,7 +32,7 @@ const removeContact = async (req, res) => {
 
 const addContact = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.create(req.body, owner);
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
